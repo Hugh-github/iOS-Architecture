a2d5f0888cde61b3a387872d6b2f0fe858139caa
Hugh <102569735+Hugh-github@users.noreply.github.com>
2023-06-17 16:29:52 +0900

    chore: 불필요한 코드 제거

--- Architecture(MVC, MVVM)/Architecture(MVC, MVVM)/MVCViewController.swift
+++ Architecture(MVC, MVVM)/Architecture(MVC, MVVM)/MVCViewController.swift
@@ -8,30 +8,17 @@
 import UIKit
 
 class MVCViewController: UIViewController {
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "MVC"
-        label.font = UIFont.preferredFont(forTextStyle: .headline)
-        label.textColor = .blue
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        return label
-    }()
+    
+    private let listView = MovieSearchView()
+    
+    override func loadView() {
+        self.view = listView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "MVC"
         view.backgroundColor = .systemBackground
-        setLayout()
-    }
-    
-    private func setLayout() {
-        self.view.addSubview(titleLabel)
-        
-        NSLayoutConstraint.activate([
-            self.titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5),
-            self.titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10)
-        ])
     }
 }
