6e8a769cbc0d282dfef94dea4eebe198c3eb25a5
Hugh <102569735+Hugh-github@users.noreply.github.com>
2023-06-20 20:54:35 +0900

    refact: EndPoint Component 수정

--- Architecture(MVC, MVVM)/Architecture(MVC, MVVM)/Networking/EndPoint.swift
+++ Architecture(MVC, MVVM)/Architecture(MVC, MVVM)/Networking/EndPoint.swift
@@ -15,13 +15,13 @@ struct EndPoint {
     private let base: BaseURL
     private let query: RequestQuery
     private let method: HTTPMethod
-    private let header: [HeaderComponents]? // 모든 case에서 header가 반드시 필요한 것은 아니다.
+    private let header: EssentailHeader // 모든 case에서 header가 반드시 필요한 것은 아니다.
     
     init(
         base: BaseURL,
         query: RequestQuery,
         method: HTTPMethod,
-        header: [HeaderComponents]? = nil
+        header: EssentailHeader
     ) {
         self.base = base
         self.query = query
@@ -52,67 +52,47 @@ struct EndPoint {
         request.httpMethod = self.method.rawValue
         
         // header 추가
-        self.header?.forEach({ header in
-            switch header {
-            case .clientID(let id):
-                request.addValue(id, forHTTPHeaderField: "X-Naver-Client-Id")
-            case .clientSecret(let secret):
-                request.addValue(secret, forHTTPHeaderField: "X-Naver-Client-Secret")
-            }
-        })
+        request.addValue(self.header.clientID, forHTTPHeaderField: "X-Naver-Client-Id")
+        request.addValue(self.header.clientSecret, forHTTPHeaderField: "X-Naver-Client-Secret")
         
         return request
     }
 }
 
 enum BaseURL: String {
-    case naverSearch = "https://openapi.naver.com/v1/search/movie.json"
+    case naverSearch = "https://openapi.naver.com/v1/search/shop.json"
 }
 
 struct RequestQuery {
-    private let movieName: String
-    private let genre: Genre?
-    private let country: Country?
+    private let itemName: String
+    private let sort: Sort
     
     init(
-        movieName: String,
-        genre: Genre?,
-        country: Country?
+        itemName: String,
+        sort: Sort
     ) {
-        self.movieName = movieName
-        self.genre = genre
-        self.country = country
+        self.itemName = itemName
+        self.sort = sort
     }
     
     
     var parameter: [String: String?] {
-        return ["query": movieName, "genre": genre?.rawValue, "country": country?.rawValue]
+        return ["query": itemName, "sort": sort.rawValue]
     }
+}
+
+enum Sort: String {
+    case sim
+    case date
+    case asc
+    case dsc
 }
 
 enum HTTPMethod: String {
     case get = "GET"
 }
 
-enum Genre: String {
-    case drama = "1"
-    case fantasy = "2"
-    case horror = "4"
-    case romance = "5"
-}
-
-enum Country: String {
-    case korea = "KR"
-    case usa = "US"
-    case japan = "JP"
-}
-
 // 두개를 묶어서 어떻게 처리할지 고민
-
-enum HeaderComponents {
-    case clientID(_ id: String)
-    case clientSecret(_ secret: String)
-}
 
 struct EssentailHeader {
     let clientID = "1RYp_vWiUKUPApHEhCCI"
